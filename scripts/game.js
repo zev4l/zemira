4035dfbdd96bfb0039358ba53eb39dc3acdb2c44
Ha-sake <57680069+Ha-sake@users.noreply.github.com>
2020-05-06 20:58:59 +0100

    Desenvolvimento de grande parte do algoritmo do jogo.
    Adicionaram-se icons novos

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,6 @@
+/* CONSTANTES */
+
+
 /* ESTADO DO JOGO */ 
 
 let estado = {
@@ -7,6 +10,30 @@ let estado = {
     currentCards: [],
 }
 
+let config = {
+    backImageSource: "imagens/testPatternCard.PNG",
+    cardPacks: {}
+}
+
+/* Other Functions */
+
+function imageSetter() {
+    
+    //Defining back image
+
+    for (i=1; i<20; i++){
+        document.getElementsByClassName("card")[i].getElementsByTagName("img")[0].src = config.backImageSource
+
+    
+}
+}
+
+function cardSourceChecker(n) {
+    return document.getElementsByClassName("card")[n].getElementsByTagName("img")[0].src
+}
+
+/* Other Functions */
+
 /* FUNÇÕES DE CONTROLO DE APRESENTAÇÃO DO JOGO/BOTÕES */
 
 function singlePlayerButton() {
@@ -15,18 +42,44 @@ function singlePlayerButton() {
     document.getElementsByClassName("multiPlayer")[0].style.display = "none"
 }
 
-function tableTester() {
-    cardTable = document.getElementsByClassName("cardTable")[0]
-    cards = cardTable.querySelectorAll("th")
-    
-
-}
-
 function showCard(n) {
     cardStyle = document.getElementsByClassName("card")[n].style
     cardStyle.transform = "rotateY(0deg)"
-    estado.currentCards += 
+    
+    if (estado.currentCards.length < 2) {
+        estado.currentCards.push(n)
+    }
+    if ((estado.currentCards.length) == 2) {
+        if (cardSourceChecker(estado.currentCards[0]) == cardSourceChecker(estado.currentCards[1])){
 
+            estado.matches += 1
+            estado.currentCards = []
+            
+        } else if (!(cardSourceChecker(estado.currentCards[0]) == cardSourceChecker(estado.currentCards[1]))) {
+
+            /* É necessário definir estes backups dos identificadores das cartas, pois caso contrário,
+            estado.currentCards estaria vazio pela altura em que setTimeout executásse o código interno */
+            
+            let tmpCardNum0 = estado.currentCards[0]
+            let tmpCardNum1 = estado.currentCards[1]
+
+            setTimeout(function() {
+                hideCard(tmpCardNum0)
+                hideCard(tmpCardNum1)
+            }, 1500)
+
+            estado.currentCards = []
+
+            
+        }
+
+
+    }
+
+function hideCard(n) {
+    cardStyle = document.getElementsByClassName("card")[n].style
+    cardStyle.transform = "rotateY(180deg)"
+}
     
     // if (cardStyle.transform == "rotateY(180deg)" || cardStyle.transform == "") {
     //     cardStyle.transform = "rotateY(0deg)"
@@ -35,9 +88,5 @@ function showCard(n) {
     //     cardStyle.transform = "rotateY(180deg)"
     // }
 
-
-
-
     
-
 }
