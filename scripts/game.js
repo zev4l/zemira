6262168da639d1096228d0062182273c2c6d989f
Ha-sake <57680069+Ha-sake@users.noreply.github.com>
2020-05-07 16:01:26 +0100

    Avanço no código base do jogo

--- scripts/game.js
+++ scripts/game.js
@@ -1,57 +1,134 @@
 /* CONSTANTES */
 
+// Referência temporária aos icones das cartas 
+
+const foodPack = [
+    "imagens/iconPacks/food/1.png",
+    "imagens/iconPacks/food/2.png",
+    "imagens/iconPacks/food/3.png",
+    "imagens/iconPacks/food/4.png",
+    "imagens/iconPacks/food/5.png",
+    "imagens/iconPacks/food/6.png",
+    "imagens/iconPacks/food/7.png",
+    "imagens/iconPacks/food/8.png",
+    "imagens/iconPacks/food/9.png",
+    "imagens/iconPacks/food/10.png",
+    "imagens/iconPacks/food/11.png",
+    "imagens/iconPacks/food/12.png",
+    "imagens/iconPacks/food/13.png",
+    "imagens/iconPacks/food/14.png",
+    "imagens/iconPacks/food/15.png",
+    "imagens/iconPacks/food/16.png",
+    "imagens/iconPacks/food/17.png",
+    "imagens/iconPacks/food/18.png",
+    "imagens/iconPacks/food/19.png",
+    "imagens/iconPacks/food/20.png"]
+
+
 
 /* ESTADO DO JOGO */ 
 
 let estado = {
     login: null,
-    matches: null,
-    timeSpent: null,
+    matches: 0,
+    startTime: null,
+    timePassed: null,
+    timerID: null,
     currentCards: [],
+    usedCards: []
 }
 
 let config = {
     backImageSource: "imagens/testPatternCard.PNG",
-    cardPacks: []
+    frontImagePackSource: foodPack
 }
 
-/* Other Functions */
+/* FUNCTIONS */
 
 function imageSetter() {
     
-    //Defining back image
+    /* Definição da imagem de trás */
 
-    for (i=1; i<20; i++){
-        document.getElementsByClassName("card")[i].getElementsByTagName("img")[0].src = config.backImageSource
+    for (i=0; i<20; i++){
+        document.getElementsByClassName("cardBack")[i].getElementsByTagName("img")[0].src = config.backImageSource
+    }
+    /* Definição das imagens da frente */
 
     
-}
+    iconPackClone = [...config.frontImagePackSource] // Clonagem do iconPack atual de modo a não alterar o original
+    let pairedIconPackClone = []
+    
+    shuffleArray(iconPackClone)
+
+    for (i=0; i<10; i++ ) { //Duplicação dos elementos no iconPack para que sejam formados pares
+        pairedIconPackClone.push(iconPackClone[i])
+        pairedIconPackClone.push(iconPackClone[i])
+    }
+
+    shuffleArray(pairedIconPackClone)
+
+    for (i=0; i<20; i++){
+        document.getElementsByClassName("cardFront")[i].getElementsByTagName("img")[0].src = pairedIconPackClone[i]
+    
+    }
+
 }
 
 function cardSourceChecker(n) {
-    return document.getElementsByClassName("card")[n].getElementsByTagName("img")[0].src
+    return document.getElementsByClassName("cardFront")[n].getElementsByTagName("img")[0].src
 }
 
 
 /* FUNÇÕES DE CONTROLO DE APRESENTAÇÃO DO JOGO/BOTÕES */
 
 function singlePlayerButton() {
-    document.getElementsByClassName("gameContent")[0].style.display = "block"
+
+    // Esconder elementos e mostar o start button
+
     document.getElementsByClassName("singlePlayer")[0].style.display = "none"
     document.getElementsByClassName("multiPlayer")[0].style.display = "none"
+
+    document.getElementsByClassName("startButton")[0].style.display = "block"
+    document.getElementsByClassName("sideBar")[0].style.display = "none"
 }
+
+function startButton () {
+    document.getElementsByClassName("gameContent")[0].style.display = "inline-block"
+    document.getElementsByClassName("cardTable")[0].style.display = "inline-block"
+    document.getElementsByClassName("sideBar")[0].style.display = "inline-block"
+    document.getElementsByClassName("startButton")[0].style.display = "none"
+    for (i=0; i<20;i++) {
+        document.getElementsByClassName("cardContainer")[i].style.visibility = "visible"
+    }
+    
+    estado.startTime = Math.floor(Date.now()/1000)
+    estado.timerID = setInterval(showtimePassed,1000)
+
+}
+
+function showtimePassed() {
+
+    estado.timePassed = Math.floor((Date.now()/1000 - estado.startTime))
+  
+    // Atualiza o temporizador visualmente.
+    document.getElementsByClassName("durationCounter")[0].getElementsByTagName("span")[0].innerHTML = estado.timePassed
+  
+  }
+
+/* FUNÇÃO PRINCIPAL */
 
 function showCard(n) {
     cardStyle = document.getElementsByClassName("card")[n].style
     cardStyle.transform = "rotateY(0deg)"
     
-    if (estado.currentCards.length < 2) {
+    if (estado.currentCards.length < 2 && n!=estado.currentCards[0] && !(estado.usedCards.includes(n))){
         estado.currentCards.push(n)
     }
     if ((estado.currentCards.length) == 2) {
-        if (cardSourceChecker(estado.currentCards[0]) == cardSourceChecker(estado.currentCards[1])){
+        if (cardSourceChecker(estado.currentCards[0]) == cardSourceChecker(estado.currentCards[1]) && estado.currentCards[0] != estado.currentCards[1] ){
 
             estado.matches += 1
+            estado.usedCards.push(estado.currentCards[0], estado.currentCards[1])
             estado.currentCards = []
             
         } else if (!(cardSourceChecker(estado.currentCards[0]) == cardSourceChecker(estado.currentCards[1]))) {
@@ -65,22 +142,34 @@ function showCard(n) {
             setTimeout(function() {
                 hideCard(tmpCardNum0)
                 hideCard(tmpCardNum1)
-            }, 1500)
+            }, 1000)
 
             estado.currentCards = []
 
             
         }
-
+        if (estado.matches ==10) {
+            clearInterval(estado.timerID)
+        }
+        // if (estado.matches == 10) {
+        //     endGame()
+        // }
+    updateMatches()
 
     }
+
+}
+
+function updateMatches() {
+    document.getElementsByClassName("matchesFound")[0].getElementsByTagName("span")[0].innerHTML = estado.matches
+}
+
 
 function hideCard(n) {
     cardStyle = document.getElementsByClassName("card")[n].style
     cardStyle.transform = "rotateY(180deg)"
 }
 
-}
 
 
 /* Uso do algoritmo Fisher-Yates-Durstenfelt para organizar uma lista de forma aleatoria */
@@ -93,4 +182,3 @@ function shuffleArray(array) {
         array[j] = temp
     }
 }
-
