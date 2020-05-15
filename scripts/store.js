1ec83c47942f6cafb8ec5823fdfb2d6330a0faf8
mike12-debug <62181571+mike12-debug@users.noreply.github.com>
2020-05-15 17:11:44 +0100

    aaa

--- scripts/store.js
+++ scripts/store.js
@@ -6,3 +6,14 @@ $(document).ready(displayPoints);
 function displayPoints() {
     document.getElementsByClassName("ZPoints")[0].getElementsByTagName("span")[0].innerHTML = localStorage.getItem('zPoints');
 }
+
+
+
+function displayAllIcons(){
+	document.getElementsByClassName("slideShowHolder")[0].style.display="flex"
+}
+
+
+function closeSlideShow(){
+	document.getElementsByClassName("slideShowHolder")[0].style.display="none"
+}
